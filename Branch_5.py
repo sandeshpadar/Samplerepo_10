cc60204183b63b9cb238c8b04652b7ce21dfa48a
sandeshpadar <sandeshpadar1@gmail.com>
2023-10-27 11:25:45 +0530

    For my fifth commit

---
+++ Branch_5.py
@@ -0,0 +1,7 @@
+def person(name = 'bhushan', city= 'delhi'):
+    print('my name is' +' ' + name +' ' + 'my  city is' + ' ' + city)
+
+person('sandesh', 'Amravati')
+person('nikhil', 'Nagpur')
+person()
+person('sakhi', 'Pune')
